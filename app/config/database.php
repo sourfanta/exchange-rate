196124b9ecbe3ecb93255164e701a85a9fc6fc8b
Dmytro <fantaigr69@gmail.com>
2023-01-22 11:45:56 +0200

    build: docker and database settings

--- app/config/database.php
+++ app/config/database.php
@@ -47,10 +47,10 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'port' => env('DB_PORT', '6033'),
+            'database' => env('DB_DATABASE', 'app_db'),
+            'username' => env('DB_USERNAME', 'db_user'),
+            'password' => env('DB_PASSWORD', 'db_user_pass'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -125,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
